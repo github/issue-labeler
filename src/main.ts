de16e742018d174ccf61d287f6ed31eb48faa64f
Balázs Orbán <info@balazsorban.com>
2023-01-27 19:17:52 +0100

    feat: bring issue-labeler up-to-date (#57)

--- src/main.ts
+++ src/main.ts
@@ -1,195 +1,163 @@
-import * as core from '@actions/core';
-import * as github from '@actions/github';
-import * as yaml from 'js-yaml';
+import { getInput, error as setError, setFailed } from "@actions/core";
+import { context, getOctokit } from "@actions/github";
+import { load as loadYaml } from "js-yaml";
 
+type GitHubClient = ReturnType<typeof getOctokit>["rest"];
+
+let configPath;
 async function run() {
-  try {
-    // Configuration parameters
-    var configPath = core.getInput('configuration-path', { required: true });
-    const token = core.getInput('repo-token', { required: true });
-    const enableVersionedRegex = parseInt(core.getInput('enable-versioned-regex', { required: true }));
-    const versionedRegex = new RegExp(core.getInput('versioned-regex', { required: false }));
-    const notBefore = Date.parse(core.getInput('not-before', { required: false }));
-    const bodyMissingRegexLabel = core.getInput('body-missing-regex-label', { required: false });
-    const includeTitle = parseInt(core.getInput('include-title', { required: false }));
+  // Configuration parameters
+  configPath = getInput("configuration-path", { required: true });
+  const token = getInput("repo-token", { required: true });
+  const enableVersionedRegex = parseInt(
+    getInput("enable-versioned-regex", { required: true })
+  );
+  const versionedRegex = new RegExp(
+    getInput("versioned-regex", { required: false })
+  );
+  const notBefore = Date.parse(getInput("not-before", { required: false }));
+  const bodyMissingRegexLabel = getInput("body-missing-regex-label", {
+    required: false,
+  });
+  const includeTitle = parseInt(getInput("include-title", { required: false }));
 
-    const issue_number = getIssueOrPullRequestNumber();
-    if (issue_number === undefined) {
-      console.log('Could not get issue or pull request number from context, exiting');
+  const issue_number = getIssueOrPRNumber();
+  if (issue_number === undefined) {
+    console.log("Could not get issue or PR number from context, exiting");
+    return;
+  }
+
+  const issue_body = getIssueOrPRBody();
+  if (!issue_body) {
+    console.log("Could not get issue or PR body from context, exiting");
+    return;
+  }
+
+  const issue_title = getIssueOrPRTitle();
+  if (!issue_title) {
+    console.log("Could not get issue or PR title from context, exiting");
+    return;
+  }
+
+  // A client to load data from GitHub
+  const { rest: client } = getOctokit(token);
+
+  const addLabel: string[] = [];
+  const removeLabelItems: string[] = [];
+
+  if (enableVersionedRegex == 1) {
+    const regexVersion = versionedRegex.exec(issue_body);
+    if (!regexVersion || !regexVersion[1]) {
+      if (bodyMissingRegexLabel) {
+        await addLabels(client, issue_number, [bodyMissingRegexLabel]);
+      }
+      console.log(
+        `Issue #${issue_number} does not contain regex version in the body of the issue, exiting.`
+      );
       return;
-    }
-
-    const issue_body = getIssueOrPullRequestBody();
-    if (issue_body === undefined) {
-      console.log('Could not get issue or pull request body from context, exiting');
-      return;
-    }
-
-    const issue_title = getIssueOrPullRequestTitle();
-    if (issue_title === undefined) {
-      console.log('Could not get issue or pull request title from context, exiting');
-      return;
-    }
-
-    // A client to load data from GitHub
-    const client = new github.GitHub(token);
-
-    const addLabel: string[] = []
-    const removeLabelItems: string[] = []
-
-    if (enableVersionedRegex == 1) {
-      const regexVersion = versionedRegex.exec(issue_body)
-      if (!regexVersion || !regexVersion[1]) {
-        if (bodyMissingRegexLabel) {
-          addLabels(client, issue_number, [bodyMissingRegexLabel]);
-        }
-        console.log(`Issue #${issue_number} does not contain regex version in the body of the issue, exiting.`)
-        return 0;
-      }
-      else {
-        if (bodyMissingRegexLabel) {
-          removeLabelItems.push(bodyMissingRegexLabel);
-        }
-      }
-      configPath = regexifyConfigPath(configPath, regexVersion[1])
-    }
-
-    // If the notBefore parameter has been set to a valid timestamp, exit if the current issue was created before notBefore
-    if (notBefore) {
-      const issue = client.issues.get({
-        owner: github.context.repo.owner,
-        repo: github.context.repo.repo,
-        issue_number: issue_number,
-      });
-      const issueCreatedAt = Date.parse((await issue).data.created_at)
-      if (issueCreatedAt < notBefore) {
-        console.log("Issue is before `notBefore` configuration parameter. Exiting...")
-        process.exit(0);
+    } else {
+      if (bodyMissingRegexLabel) {
+        removeLabelItems.push(bodyMissingRegexLabel);
       }
     }
+    configPath = regexifyConfigPath(configPath, regexVersion[1]);
+  }
 
-    // Load our regex rules from the configuration path
-    const labelRegexes: Map<string, string[]> = await getLabelRegexes(
-      client,
-      configPath
-    );
-
-    let issueContent = ""
-    if (includeTitle === 1) {
-      issueContent += `${issue_title}\n\n`
-    }
-    issueContent += issue_body
-
-    for (const [label, globs] of labelRegexes.entries()) {
-      if (checkRegexes(issueContent, globs)) {
-        addLabel.push(label)
-      }
-      else {
-        removeLabelItems.push(label)
-      }
-    }
-    if (addLabel.length > 0) {
-      console.log(`Adding labels ${addLabel.toString()} to issue #${issue_number}`)
-      addLabels(client, issue_number, addLabel)
-    }
-
-    removeLabelItems.forEach(function (label, index) {
-      console.log(`Removing label ${label} from issue #${issue_number}`)
-      removeLabel(client, issue_number, label)
+  // If the notBefore parameter has been set to a valid timestamp, exit if the current issue was created before notBefore
+  if (notBefore) {
+    const issue = await client.issues.get({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      issue_number,
     });
-  } catch (error) {
-    core.error(error);
-    core.setFailed(error.message);
+    const issueCreatedAt = Date.parse(issue.data.created_at);
+    if (issueCreatedAt < notBefore) {
+      console.log(
+        "Issue is before `notBefore` configuration parameter. Exiting..."
+      );
+      return;
+    }
   }
+
+  // Load our regex rules from the configuration path
+  const labelRegexes = await loadConfig(client, configPath);
+
+  let issueContent = "";
+  if (includeTitle === 1) issueContent += `${issue_title}\n\n`;
+  issueContent += issue_body;
+
+  for (const [label, globs] of labelRegexes.entries()) {
+    if (checkRegexes(issueContent, globs)) {
+      addLabel.push(label);
+    } else {
+      removeLabelItems.push(label);
+    }
+  }
+
+  const promises = [];
+  if (addLabel.length) {
+    console.log(`Adding labels ${addLabel} to issue #${issue_number}`);
+    promises.push(addLabels(client, issue_number, addLabel));
+  }
+
+  await Promise.all(
+    promises.concat(removeLabelItems.map(removeLabel(client, issue_number)))
+  );
 }
 
-function getIssueOrPullRequestNumber(): number | undefined {
-  const issue = github.context.payload.issue;
-  if (issue) {
-    return issue.number;
-  }
-
-  const pull_request = github.context.payload.pull_request;
-  if (pull_request) {
-    return pull_request.number;
-  }
-
-  return;
+function getIssueOrPRNumber() {
+  const { issue, pull_request } = context.payload;
+  return issue?.number ?? pull_request?.number;
 }
 
-function getIssueOrPullRequestBody(): string | undefined {
-  const issue = github.context.payload.issue;
-  if (issue) {
-    return issue.body;
-  }
-
-  const pull_request = github.context.payload.pull_request;
-  if (pull_request) {
-    return pull_request.body;
-  }
-
-  return;
+function getIssueOrPRBody() {
+  const { issue, pull_request } = context.payload;
+  return issue?.body ?? pull_request?.body;
 }
 
-function getIssueOrPullRequestTitle(): string | undefined {
-  const issue = github.context.payload.issue;
-  if (issue) {
-    return issue.title;
-  }
-
-  const pull_request = github.context.payload.pull_request;
-  if (pull_request) {
-    return pull_request.title;
-  }
-
-  return;
+function getIssueOrPRTitle(): string | undefined {
+  const { issue, pull_request } = context.payload;
+  return issue?.title ?? pull_request?.title;
 }
 
 function regexifyConfigPath(configPath: string, version: string) {
-  var lastIndex = configPath.lastIndexOf('.')
-  return `${configPath.substring(0, lastIndex)}-v${version}.yml`
+  const lastIndex = configPath.lastIndexOf(".");
+  return `${configPath.substring(0, lastIndex)}-v${version}.yml`;
 }
 
-async function getLabelRegexes(
-  client: github.GitHub,
-  configurationPath: string
-): Promise<Map<string, string[]>> {
-  const configurationContent: string = await fetchContent(
-    client,
-    configurationPath
-  );
+/** Load the configuration file */
+async function loadConfig(client: GitHubClient, configPath: string) {
+  try {
+    const { data } = await client.repos.getContent({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      ref: context.sha,
+      path: configPath,
+    });
 
-  // loads (hopefully) a `{[label:string]: string | string[]}`, but is `any`:
-  const configObject: any = yaml.safeLoad(configurationContent);
+    if (!("content" in data)) {
+      throw new TypeError(
+        "The configuration path provided is not a valid file. Exiting"
+      );
+    }
 
-  // transform `any` => `Map<string,string[]>` or throw if yaml is malformed:
-  return getLabelRegexesMapFromObject(configObject);
-}
+    const configContent = Buffer.from(data.content, "base64").toString("utf8");
 
-// Load the configuration file
-async function fetchContent(
-  client: github.GitHub,
-  repoPath: string
-): Promise<string> {
-  const response = await client.repos.getContents({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    path: repoPath,
-    ref: github.context.sha
-  });
+    // loads (hopefully) a `{[label:string]: string | string[]}`, but is `any`:
+    const configObject = loadYaml(configContent);
 
-  const data: any = response.data
-  if (!data.content) {
-    console.log('The configuration path provided is not a valid file. Exiting')
-    process.exit(1);
+    // transform `any` => `Map<string,string[]>` or throw if yaml is malformed:
+    return getLabelRegexesMapFromObject(configObject);
+  } catch (error) {
+    console.log("Error loading configuration file: " + error);
+    throw error;
   }
-  return Buffer.from(data.content, 'base64').toString('utf8');
 }
 
-function getLabelRegexesMapFromObject(configObject: any): Map<string, string[]> {
-  const labelRegexes: Map<string, string[]> = new Map();
+function getLabelRegexesMapFromObject(configObject: any) {
+  const labelRegexes = new Map<string, string[]>();
   for (const label in configObject) {
-    if (typeof configObject[label] === 'string') {
+    if (typeof configObject[label] === "string") {
       labelRegexes.set(label, [configObject[label]]);
     } else if (Array.isArray(configObject[label])) {
       labelRegexes.set(label, configObject[label]);
@@ -203,17 +171,17 @@ function getLabelRegexesMapFromObject(configObject: any): Map<string, string[]> 
   return labelRegexes;
 }
 
-function checkRegexes(issue_body: string, regexes: string[]): boolean {
-  var found;
+function checkRegexes(issue_body: string, regexes: string[]) {
+  let found;
 
   // If several regex entries are provided we require all of them to match for the label to be applied.
   for (const regEx of regexes) {
-    const isRegEx = regEx.match(/^\/(.+)\/(.*)$/)
+    const isRegEx = regEx.match(/^\/(.+)\/(.*)$/);
 
     if (isRegEx) {
-      found = issue_body.match(new RegExp(isRegEx[1], isRegEx[2]))
+      found = issue_body.match(new RegExp(isRegEx[1], isRegEx[2]));
     } else {
-      found = issue_body.match(regEx)
+      found = issue_body.match(regEx);
     }
 
     if (!found) {
@@ -223,52 +191,44 @@ function checkRegexes(issue_body: string, regexes: string[]): boolean {
   return true;
 }
 
-async function getLabels(
-  client: github.GitHub,
-  issue_number: number,
-): Promise<string[]> {
-  const response = await client.issues.listLabelsOnIssue({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    issue_number: issue_number,
-  });
-  const data = response.data
-  if (response.status != 200) {
-    console.log('Unable to load labels. Exiting...')
-    process.exit(1);
-  }
-  const labels: string[] = [];
-  for (let i = 0; i < Object.keys(data).length; i++) {
-    labels.push(data[i].name)
-  }
-  return labels;
-}
-
 async function addLabels(
-  client: github.GitHub,
+  client: GitHubClient,
   issue_number: number,
   labels: string[]
 ) {
-
-  await client.issues.addLabels({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    issue_number: issue_number,
-    labels: labels
-  });
+  try {
+    console.log(`Adding labels ${labels} to issue #${issue_number}`);
+    return await client.issues.addLabels({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      issue_number,
+      labels,
+    });
+  } catch (error) {
+    console.log(`Could not add label(s) ${labels} to issue #${issue_number}`);
+    throw error;
+  }
 }
 
-async function removeLabel(
-  client: github.GitHub,
-  issue_number: number,
-  name: string
-) {
-  await client.issues.removeLabel({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    issue_number: issue_number,
-    name: name
-  });
+function removeLabel(client: GitHubClient, issue_number: number) {
+  return async function (name: string) {
+    try {
+      console.log(`Removing label ${name} from issue #${issue_number}`);
+      return await client.issues.removeLabel({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        issue_number,
+        name,
+      });
+    } catch (error) {
+      console.log(`Could not remove label ${name} from issue #${issue_number}`);
+      throw error;
+    }
+  };
 }
 
-run();
+run().catch((e) => {
+  const error = e as Error;
+  setError(error);
+  setFailed(error.message);
+});
