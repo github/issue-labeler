a695dbaa422c304288460b4d26235878e790ae01
Joao Pedro <joao@arcane.run>
2023-11-28 17:45:55 +0100

    feat: first action perchance

--- src/main.ts
+++ src/main.ts
@@ -11,10 +11,10 @@ async function run() {
   configPath = getInput("configuration-path", { required: true });
   const token = getInput("repo-token", { required: true });
   const enableVersionedRegex = parseInt(
-    getInput("enable-versioned-regex", { required: true })
+    getInput("enable-versioned-regex", { required: true }),
   );
   const versionedRegex = new RegExp(
-    getInput("versioned-regex", { required: false })
+    getInput("versioned-regex", { required: false }),
   );
   const notBefore = Date.parse(getInput("not-before", { required: false }));
   const bodyMissingRegexLabel = getInput("body-missing-regex-label", {
@@ -24,7 +24,7 @@ async function run() {
   const includeBody = parseInt(getInput("include-body", { required: false }));
   const syncLabels = parseInt(getInput("sync-labels", { required: false }));
 
-  const issue_number = parseInt(getInput("issue-number", { required: true }))
+  const issue_number = parseInt(getInput("issue-number", { required: true }));
 
   // A client to load data from GitHub
   const { rest: client } = getOctokit(token);
@@ -52,9 +52,9 @@ async function run() {
     return;
   }
 
-  const labels: String[] = []
+  const labels: String[] = [];
   issue.data.labels.forEach((label) => {
-    if (typeof label === 'string') {
+    if (typeof label === "string") {
     } else {
       labels.push(<String>label.name);
     }
@@ -69,7 +69,7 @@ async function run() {
         await addLabels(client, issue_number, [bodyMissingRegexLabel]);
       }
       console.log(
-        `Issue #${issue_number} does not contain regex version in the body of the issue, exiting.`
+        `Issue #${issue_number} does not contain regex version in the body of the issue, exiting.`,
       );
       return;
     } else if (bodyMissingRegexLabel) {
@@ -83,7 +83,7 @@ async function run() {
     const issueCreatedAt = Date.parse(issue.data.created_at);
     if (issueCreatedAt < notBefore) {
       console.log(
-        "Issue is before `notBefore` configuration parameter. Exiting..."
+        "Issue is before `notBefore` configuration parameter. Exiting...",
       );
       return;
     }
@@ -119,7 +119,7 @@ async function run() {
     .filter(Boolean);
 
   if (rejected.length) {
-    throw new AggregateError(rejected)
+    throw new AggregateError(rejected);
   }
 
   if (toAdd.length) {
@@ -134,7 +134,7 @@ async function run() {
 function getIssueOrPRBody() {
   const { issue, pull_request } = context.payload;
   if (issue?.body === null || pull_request?.body === null) {
-    return '';
+    return "";
   }
   return issue?.body ?? pull_request?.body;
 }
@@ -152,14 +152,18 @@ function regexifyConfigPath(configPath: string, version: string) {
 /** Load the configuration file */
 export async function loadConfig(client: GitHubClient, configPath: string) {
   try {
-    let configContent: string
+    let configContent: string;
 
     if (fs.existsSync(configPath)) {
-      console.log(`Configuration file (path: ${configPath}) exists locally, loading from file`);
+      console.log(
+        `Configuration file (path: ${configPath}) exists locally, loading from file`,
+      );
 
       configContent = fs.readFileSync(configPath, { encoding: "utf8" });
     } else {
-      console.log(`Configuration file (path: ${configPath}) does not exist locally, fetching via the API`);
+      console.log(
+        `Configuration file (path: ${configPath}) does not exist locally, fetching via the API`,
+      );
 
       const { data } = await client.repos.getContent({
         owner: context.repo.owner,
@@ -170,7 +174,7 @@ export async function loadConfig(client: GitHubClient, configPath: string) {
 
       if (!("content" in data)) {
         throw new TypeError(
-          "The configuration path provided is not a valid file. Exiting"
+          "The configuration path provided is not a valid file. Exiting",
         );
       }
 
@@ -189,9 +193,9 @@ export async function loadConfig(client: GitHubClient, configPath: string) {
 }
 
 interface RuleSet {
-  oneOf: string[]
-  allOf: string[]
-  exclude: string[]
+  oneOf: string[];
+  allOf: string[];
+  exclude: string[];
 }
 
 function getLabelRegexesMapFromObject(configObject: any) {
@@ -201,17 +205,20 @@ function getLabelRegexesMapFromObject(configObject: any) {
       oneOf: [],
       allOf: [],
       exclude: [],
-    }
+    };
     const value = configObject[label];
     if (typeof value === "string") {
       labelRegexes.set(label, { oneOf: [value], allOf: [], exclude: [] });
     } else if (Array.isArray(value)) {
-      labelRegexes.set(label, { oneOf: value , allOf: [], exclude: []});
-    } else if (typeof value === 'object' && (value.oneOf || value.allOf || value.exclude)) {
-      labelRegexes.set(label, {...baseRuleSet, ...value});
+      labelRegexes.set(label, { oneOf: value, allOf: [], exclude: [] });
+    } else if (
+      typeof value === "object" &&
+      (value.oneOf || value.allOf || value.exclude)
+    ) {
+      labelRegexes.set(label, { ...baseRuleSet, ...value });
     } else {
       throw Error(
-        `found unexpected type for label ${label} (should be string or array of regex)`
+        `found unexpected type for label ${label} (should be string or array of regex)`,
       );
     }
   }
@@ -262,18 +269,22 @@ export function checkRegexes(issue_body: string, regexes: RuleSet) {
 
   // If any regex in includeAny matches or all regexes in includeAll match, it's valid
   // If any regex in excludeAny matches, it's not valid
-  if ((foundsIncludeAny.some((found) => found) || (foundsIncludeAll.length > 0 && foundsIncludeAll.every((found) => found))) && !foundsExcludeAny.some((found) => found)) {
+  if (
+    (foundsIncludeAny.some((found) => found) ||
+      (foundsIncludeAll.length > 0 &&
+        foundsIncludeAll.every((found) => found))) &&
+    !foundsExcludeAny.some((found) => found)
+  ) {
     return true;
   }
 
   return false;
 }
 
-
 async function addLabels(
   client: GitHubClient,
   issue_number: number,
-  labels: string[]
+  labels: string[],
 ) {
   try {
     const formatted = labels.map((l) => `"${l}"`).join(", ");
@@ -302,7 +313,7 @@ function removeLabel(client: GitHubClient, issue_number: number) {
       });
     } catch (error) {
       console.log(
-        `Could not remove label "${name}" from issue #${issue_number}`
+        `Could not remove label "${name}" from issue #${issue_number}`,
       );
       throw error;
     }
--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,51 +1,50 @@
-import { checkRegexes, loadConfig } from '../src/main';
+import { checkRegexes, loadConfig } from "../src/main";
 
-
-
-describe('TODO - Add a test suite', () => {
-
-  it('Should extra labels', async () => {
-    const labelRegexes = await loadConfig(undefined as any,'tests/extraLabels.yml');
-    const issueContents = [
-      "A/index.html",
-      "B/index.html",
-      "C/index.html",
-    ];
+describe("TODO - Add a test suite", () => {
+  it("Should extra labels", async () => {
+    const labelRegexes = await loadConfig(
+      undefined as any,
+      "tests/extraLabels.yml",
+    );
+    const issueContents = ["A/index.html", "B/index.html", "C/index.html"];
 
     for (const issueContent of issueContents) {
       let toAdd: string[] = [];
       for (const [label, globs] of labelRegexes.entries()) {
-        console.log(label)
+        console.log(label);
         if (checkRegexes(issueContent, globs)) {
           toAdd.push(label);
         }
       }
-      expect(toAdd).toContain("Label 1")
+      expect(toAdd).toContain("Label 1");
     }
   });
 
-
-  it('Should exclude bad labels', async () => {
-    const labelRegexes = await loadConfig(undefined as any,'tests/extraLabels.yml');
-    const issueContents = [
-      "A/substuff/index.html",
-    ];
+  it("Should exclude bad labels", async () => {
+    const labelRegexes = await loadConfig(
+      undefined as any,
+      "tests/extraLabels.yml",
+    );
+    const issueContents = ["A/substuff/index.html"];
 
     for (const issueContent of issueContents) {
       let toAdd: string[] = [];
       for (const [label, globs] of labelRegexes.entries()) {
-        console.log(label)
+        console.log(label);
         if (checkRegexes(issueContent, globs)) {
           toAdd.push(label);
         }
       }
-      expect(toAdd).not.toContain("Label 1")
-      expect(toAdd).toContain("Label 2")
+      expect(toAdd).not.toContain("Label 1");
+      expect(toAdd).toContain("Label 2");
     }
   });
 
-  it('Should add a allOf labels very specific', async () => {
-    const labelRegexes = await loadConfig(undefined as any,'tests/extraLabels.yml');
+  it("Should add a allOf labels very specific", async () => {
+    const labelRegexes = await loadConfig(
+      undefined as any,
+      "tests/extraLabels.yml",
+    );
     const issueContents = [
       "This issue has a permalink to a file B/substuff/index.html and also has the magic word ABLU",
     ];
@@ -53,17 +52,14 @@ describe('TODO - Add a test suite', () => {
     for (const issueContent of issueContents) {
       let toAdd: string[] = [];
       for (const [label, globs] of labelRegexes.entries()) {
-        console.log(label)
+        console.log(label);
         if (checkRegexes(issueContent, globs)) {
           toAdd.push(label);
         }
       }
-      expect(toAdd).not.toContain("Label 1")
-      expect(toAdd).not.toContain("Label 2")
-      expect(toAdd).toContain("ABLU LABEL")
+      expect(toAdd).not.toContain("Label 1");
+      expect(toAdd).not.toContain("Label 2");
+      expect(toAdd).toContain("ABLU LABEL");
     }
   });
-
-
-
 });
